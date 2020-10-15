ec56b4e41837ad515cec0dfcc5dd60656aee759d
Jayson Obos <jayson.obos@lkg.com.au>
2020-10-16 02:55:59 +0800

    header current progress

--- wp-content/themes/yourtheme/template-parts/burgermenu.php
+++ wp-content/themes/yourtheme/template-parts/burgermenu.php
@@ -8,6 +8,5 @@
 </div>
 
 <div class="burger-content vh-100 w-100 text-center d-md-none">
-    <h3>test</h3>
-    <?php wp_nav_menu(array('theme_location' => 'header-menu', 'fallback_cb' => 'false')); ?>
+    <?php wp_nav_menu(array('theme_location' => 'primary', 'fallback_cb' => 'false')); ?>
 </div>
