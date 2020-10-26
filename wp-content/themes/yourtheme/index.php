e68f1077b9abfcdb436edeb50f4a104e4208f210
Jayson Obos <jayson.obos@lkg.com.au>
2020-10-27 00:49:32 +0800

    added Neighborhood links section

--- wp-content/themes/yourtheme/index.php
+++ wp-content/themes/yourtheme/index.php
@@ -1,21 +1,14 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
+		<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+		<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('loop'); ?>
+		<?php get_template_part('pagination'); ?>
 
-			<?php get_template_part('pagination'); ?>
-
-			<?php get_template_part('template-parts/testfont'); ?>
-
-		</section>
-		<!-- /section -->
+		<?php get_template_part('template-parts/home/nblinks'); ?>
 	</main>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/yourtheme/template-parts/home/nblinks.php
@@ -0,0 +1,21 @@
+<section class="nblinks">
+    <div class="container">
+        <div class="d-lg-flex">
+            <a class="w-100 col-lg-4 d-flex align-items-center justify-content-center position-relative" href="#">
+                <div class="position-relative text-center">
+                    <p class="position-absolute">LIST WITH US</p>
+                </div>
+            </a>
+            <a class="w-100 col-lg-4 d-flex align-items-center justify-content-center position-relative" href="#">
+                <div class="position-relative text-center">
+                    <p class="position-absolute">NEIGHBORHOOD GUIDES</p>
+                </div>
+            </a>
+            <a class="w-100 col-lg-4 d-flex align-items-center justify-content-center" href="#">
+                <div class="position-relative text-center">
+                    <p class="position-absolute">CONTACT DETAILS</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
